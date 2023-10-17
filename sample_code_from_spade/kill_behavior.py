d49ce7c7782515c4426a789bbc59d290fb0266ee
Antonio de Castro <frisky.antonio@gmail.com>
2023-10-17 16:10:25 +0100

    Adding example code from spade

---
+++ sample_code_from_spade/kill_behavior.py
@@ -0,0 +1,52 @@
+import asyncio
+import getpass
+
+import spade
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+
+
+class DummyAgent(Agent):
+    class MyBehav(CyclicBehaviour):
+        async def on_start(self):
+            print("Starting behaviour . . .")
+            self.counter = 0
+
+        async def run(self):
+            print("Counter: {}".format(self.counter))
+            self.counter += 1
+            if self.counter > 3:
+                self.kill(exit_code=10)
+                return
+            await asyncio.sleep(1)
+
+        async def on_end(self):
+            print("Behaviour finished with exit code {}.".format(self.exit_code))
+
+    async def setup(self):
+        print("Agent starting . . .")
+        self.my_behav = self.MyBehav()
+        self.add_behaviour(self.my_behav)
+
+
+async def main():
+    jid = input("JID> ")
+    passwd = getpass.getpass()
+
+    dummy = DummyAgent(jid, passwd)
+    await dummy.start()
+
+    # wait until user interrupts with ctrl+C
+    while not dummy.my_behav.is_killed():
+        try:
+            await asyncio.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+    assert dummy.my_behav.exit_code == 10
+
+    await dummy.stop()
+
+
+if __name__ == "__main__":
+    spade.run(main())
---
+++ sample_code_from_spade/timeout_example.py
@@ -0,0 +1,67 @@
+import datetime
+import getpass
+
+import spade
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour, TimeoutBehaviour
+from spade.message import Message
+
+
+class TimeoutSenderAgent(Agent):
+    class InformBehav(TimeoutBehaviour):
+        async def run(self):
+            print(f"TimeoutSenderBehaviour running at {datetime.datetime.now().time()}")
+            msg = Message(to=self.get("receiver_jid"))  # Instantiate the message
+            msg.body = "Hello World"  # Set the message content
+
+            await self.send(msg)
+
+        async def on_end(self):
+            await self.agent.stop()
+
+    async def setup(self):
+        print(f"TimeoutSenderAgent started at {datetime.datetime.now().time()}")
+        start_at = datetime.datetime.now() + datetime.timedelta(seconds=5)
+        b = self.InformBehav(start_at=start_at)
+        self.add_behaviour(b)
+
+
+class ReceiverAgent(Agent):
+    class RecvBehav(CyclicBehaviour):
+        async def run(self):
+            msg = await self.receive(timeout=10)  # wait for a message for 10 seconds
+            if msg:
+                print("Message received with content: {}".format(msg.body))
+            else:
+                print("Did not received any message after 10 seconds")
+            self.kill()
+
+        async def on_end(self):
+            await self.agent.stop()
+
+    async def setup(self):
+        b = self.RecvBehav()
+        self.add_behaviour(b)
+
+
+async def main():
+    receiver_jid = input("Receiver JID> ")
+    passwd = getpass.getpass()
+    receiveragent = ReceiverAgent(receiver_jid, passwd)
+
+    sender_jid = input("Sender JID> ")
+    passwd = getpass.getpass()
+    senderagent = TimeoutSenderAgent(sender_jid, passwd)
+
+    await receiveragent.start(auto_register=True)
+
+    # store receiver_jid in the sender knowledge base
+    senderagent.set("receiver_jid", receiver_jid)
+    await senderagent.start(auto_register=True)
+
+    await spade.wait_until_finished(receiveragent)
+    print("Agents finished")
+
+
+if __name__ == "__main__":
+    spade.run(main())
